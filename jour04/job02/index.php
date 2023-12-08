62d90efc434cec53bd5e988ae937830b6b66af23
Alina83200 <alina.yefimova@laplateforme.io>
2023-12-08 16:19:13 +0100

    rr

--- jour04/job02/index.php
+++ jour04/job02/index.php
@@ -8,42 +8,7 @@
 </head>
 
 <body>
-    <style>
-        table {
-            border-collapse: collapse;
-            width: 50%;
-            margin: 20px;
-        }
-
-        th,
-        td {
-            border: 1px solid #ddd;
-            padding: 8px;
-            text-align: left;
-        }
-
-        th {
-            background-color: #f2f2f2;
-        }
-    </style>
-    <form action="" method="GET">
-        <label for="nom">Nom :</label>
-        <input type="text" id="nom" name="nom"></br>
-        <label for="prenom">Prenom :</label>
-        <input type="text" id="prenom" name="prenom"></br>
-        <input type="submit" value="Envoyer">
-    </form>
-
-    <?php
-
-    echo '<table>';
-    echo '<tr><th>Argument</th><th>Valeur</th></tr>';
-
-    foreach ($_GET as $key => $value) {
-        echo '<tr><td>' . $key . '</td><td>' . $value . '</td></tr>';
-    }
-    echo '</table>';
-    ?>
+    $dat
 </body>
 
 </html>
